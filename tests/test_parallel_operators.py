aabe29b94ee2bf661499331ce8e365df011d50de
Michael Dales <mwd24@cam.ac.uk>
2025-04-11 11:52:33 +0100

    Add some more tests

--- tests/test_parallel_operators.py
+++ tests/test_parallel_operators.py
@@ -65,6 +65,38 @@ def test_parallel_with_different_skip(skip, expected_steps) -> None:
 
         assert callback_possitions == expected_steps
 
+def test_parallel_equality() -> None:
+    with tempfile.TemporaryDirectory() as tempdir:
+        path1 = os.path.join(tempdir, "test1.tif")
+        data1 = np.array([[1, 2, 3, 4], [4, 3, 2, 1]])
+        dataset1 = gdal_dataset_with_data((0.0, 0.0), 0.02, data1, filename=path1)
+        dataset1.Close()
+        with RasterLayer.layer_from_file(path1) as layer1:
+            with RasterLayer.empty_raster_layer_like(layer1) as result:
+                comp = layer1 == 2
+                comp.parallel_save(result)
+
+                expected = np.array([[0, 1, 0, 0], [0, 0, 1, 0]])
+                actual = result.read_array(0, 0, 4, 2)
+
+                assert (expected == actual).all()
+
+def test_parallel_equality_to_file() -> None:
+    with tempfile.TemporaryDirectory() as tempdir:
+        path1 = os.path.join(tempdir, "test1.tif")
+        path2 = os.path.join(tempdir, "result.tif")
+        data1 = np.array([[1, 2, 3, 4], [4, 3, 2, 1]])
+        dataset1 = gdal_dataset_with_data((0.0, 0.0), 0.02, data1, filename=path1)
+        dataset1.Close()
+        with RasterLayer.layer_from_file(path1) as layer1:
+            comp = layer1 == 2
+            with RasterLayer.empty_raster_layer_like(layer1, filename=path2) as result:
+                comp.parallel_save(result)
+        with RasterLayer.layer_from_file(path2) as actual_result:
+            expected = np.array([[0, 1, 0, 0], [0, 0, 1, 0]])
+            actual = actual_result.read_array(0, 0, 4, 2)
+            assert (expected == actual).all()
+
 def test_parallel_unary_numpy_apply_with_function() -> None:
     with tempfile.TemporaryDirectory() as tempdir:
         path1 = os.path.join(tempdir, "test1.tif")
@@ -113,13 +145,13 @@ def test_parallel_where_simple() -> None:
         dataset1 = gdal_dataset_with_data((0.0, 0.0), 0.02, data1, filename=path1)
         dataset1.Close()
         layer1 = RasterLayer.layer_from_file(path1)
-        
+
         result = RasterLayer.empty_raster_layer_like(layer1)
-    
+
         comp = LayerOperation.where(layer1 > 0, 1, 2)
         comp.ystep = 1
         comp.parallel_save(result)
-    
+
         expected = np.where(data1 > 0, 1, 2)
         actual = result.read_array(0, 0, 4, 2)
         assert (expected == actual).all()
