29a660e6eecaadaabde6260fe8eda5dc110ed252
Michael Dales <mwd24@cam.ac.uk>
2025-09-27 08:41:42 +0100

    Make projection tests not had WGS hard coded

--- tests/test_base.py
+++ tests/test_base.py
@@ -84,70 +84,83 @@ def test_latlng_for_pixel(
     assert math.isclose(result[1], expected[1])
 
 @pytest.mark.parametrize(
-    "area,coord,expected",
+    "area,projection,coord,expected",
     [
         (
             Area(-10, 10, 10, -10),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (10.0, -10.0),
             (0, 0)
         ),
         (
             Area(-10, 10, 10, -10),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (9.8, -9.8),
             (1, 1)
         ),
         (
             Area(-10, 10, 10, -10),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (0.0, 0.0),
             (50, 50)
         ),
     ]
 )
-def test_pixel_for_latlng(area: Area, coord: tuple[float, float], expected: tuple[int, int]) -> None:
+def test_pixel_for_latlng(
+    area: Area,
+    projection: MapProjection,
+    coord: tuple[float, float],
+    expected: tuple[int, int]
+) -> None:
     layer = YirgacheffeLayer(
         area,
-        MapProjection(yg.WGS_84_PROJECTION, 0.2, -0.2),
+        projection,
     )
     result = layer.pixel_for_latlng(*coord)
     assert result == expected
 
-
 @pytest.mark.parametrize(
-    "area,window,pixel,expected",
+    "area,window,projection,pixel,expected",
     [
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (0, 0),
             (5.0, -5.0)
         ),
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (1, 1),
             (4.8, -4.8)
         ),
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (101, 101),
             (-15.2, 15.2)
         ),
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (-1, -1),
             (5.2, -5.2)
         ),
         (
             Area(10, 10, 20, -10),
             Area(15, 5, 20, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (1, 1),
             (4.8, 15.2)
         ),
         (
             Area(-10, -10, 10, -20),
             Area(-5, -15, 5, -20),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (1, 1),
             (-15.2, -4.8)
         ),
@@ -156,12 +169,13 @@ def test_pixel_for_latlng(area: Area, coord: tuple[float, float], expected: tupl
 def test_latlng_for_pixel_with_intersection(
     area: Area,
     window: Area,
+    projection: MapProjection,
     pixel: tuple[int, int],
     expected: tuple[float, float]
 ) -> None:
     layer = YirgacheffeLayer(
         area,
-        MapProjection(yg.WGS_84_PROJECTION, 0.2, -0.2),
+        projection,
     )
     layer.set_window_for_intersection(window)
     result = layer.latlng_for_pixel(*pixel)
@@ -169,23 +183,26 @@ def test_latlng_for_pixel_with_intersection(
     assert math.isclose(result[1], expected[1])
 
 @pytest.mark.parametrize(
-    "area,window,coord,expected",
+    "area,window,projection,coord,expected",
     [
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (5.0, -5.0),
             (0, 0)
         ),
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (4.8, -4.8),
             (1, 1)
         ),
         (
             Area(-10, 10, 10, -10),
             Area(-5, 5, 5, -5),
+            MapProjection("epsg:4326", 0.2, -0.2),
             (0.0, 0.0),
             (25, 25)
         ),
@@ -194,12 +211,13 @@ def test_latlng_for_pixel_with_intersection(
 def test_pixel_for_latlng_with_intersection(
     area: Area,
     window: Area,
+    projection: MapProjection,
     coord: tuple[float, float],
     expected: tuple[int, int]
 ) -> None:
     layer = YirgacheffeLayer(
         area,
-        MapProjection(yg.WGS_84_PROJECTION, 0.2, -0.2),
+        projection,
     )
     layer.set_window_for_intersection(window)
     result = layer.pixel_for_latlng(*coord)
